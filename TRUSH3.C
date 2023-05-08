644cda988f06977e0e283f0247faa605aa8591fc
tspatel <132879158+tspatel@users.noreply.github.com>
2023-05-08 13:08:29 +0530

    Add files via upload

---
+++ TRUSH3.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{   clrscr();
+    printf("my name is trush");
+    printf("\nmy age is 16");
+    printf("\nmy school name is ashadeep vidhyalaya");
+    getch();
+    }
